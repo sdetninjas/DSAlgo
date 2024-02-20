93149eb38a033ddcb8871c08517a9ba90022feac
sdetninjas <judefebi@gmail.com>
2024-02-20 17:23:27 -0600

    updated for Array

--- src/test/java/com/pages/ArrayPage.java
+++ src/test/java/com/pages/ArrayPage.java
@@ -1,5 +1,7 @@
 package com.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
@@ -7,21 +9,35 @@ public class ArrayPage extends BasePage{
 	
 	
 	@FindBy (xpath = "//*[@href='array']")
-	WebElement arrayGetStartedBtn;
+	private WebElement arrayGetStartedBtn;
+	@FindBy (xpath= "//*[@href='/tryEditor']")
+	private WebElement tryEditorBtn;
 
 	@Override
 	public void openPage() {
 		driver.get(BASE_URL + "array");
 	}
 	
-//	public void clickLink(WebElement we) {
-//		we.click();
-//		
-//	}
+	public void clickByName(String pagename) {
+		String newName = pagename.toLowerCase().replaceAll(" " , "-");
+		String newXpath = "//*[@href='"+newName	+ "']" ;
+		waitFor(1000);
+		WebElement pageWe = driver.findElement(By.xpath(newXpath));
+		
+		((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);", pageWe);
+		waitFor(800);
+		pageWe.click();
+	}
 	
 	public void clickarrayGetStartedBtn() {
 		arrayGetStartedBtn.click();
 		
 	}
-
+	
+	public void clickTryEditorBtn() {
+		tryEditorBtn.click();
+		
+	}
+	
+	
 }

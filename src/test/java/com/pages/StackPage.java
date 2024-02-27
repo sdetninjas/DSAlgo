9e26b5f2ddb97ffff1160f7192f81f7262af540a
deepathalaivan <96507878+deepathalaivan@users.noreply.github.com>
2024-02-26 21:26:38 -0600

    datastructure stack queue features

--- src/test/java/com/pages/StackPage.java
+++ src/test/java/com/pages/StackPage.java
@@ -1,5 +1,31 @@
 package com.pages;
 
-public class StackPage {
+import org.openqa.selenium.By;
 
+public class StackPage extends BasePage{
+	private By signIn = By.xpath("//a[@href='/login']");
+	private By userName = By.name("username");
+	private By password = By.name("password");
+	private By login = By.xpath("//input[@wfd-id='id4']");
+	private By getStarted = By.xpath("//a[@href='stack']");
+	private By stackPage = By.xpath("//h4[@class=\"bg-secondary text-white\"]");
+	private By operationalInStack = By.xpath("");
+	private By operationalInStackPage = By.xpath("//p[@class='bg-secondary text-white' and @align='center']");
+	private By tryHere = By.xpath("//a[@href='/tryEditor']");
+	private By application = By.xpath("//a[@href='/stack/stack-applications/']");
+	private By applicationPage = By.xpath("//p[text()='Stack' and @align='center']");
+	private By tryHere1 = By.xpath("//a[@href='/tryEditor']");
+	private By implementation = By.xpath("/stack/implementation/");
+	private By implementationPage = By.xpath("//p[@class='bg-secondary text-white' and @align='center']");
+	private By tryHere2 = By.xpath("//a[@href='/tryEditor']");
+	private By practiceQuestion = By.xpath("//a[@class='list-group-item list-group-item-light text-info']");
+	private By practiceQuestionPage = By.xpath("//a[@class='navbar-brand']");
+	
+	
+	
+	@Override
+	public void openPage() {
+		// TODO Auto-generated method stub
+		
+	}
 }

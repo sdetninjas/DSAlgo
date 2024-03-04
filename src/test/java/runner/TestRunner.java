b6080cab23db3472597178e0f6936a357ea74cdf
sdetninjas <judefebi@gmail.com>
2024-03-03 22:43:45 -0600

    test runner update

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,7 +6,6 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 	
-//@RunWith(Cucumber.class) //Junit execution
 
 	@CucumberOptions(
 			plugin = {"pretty", "html:target/cucumber-reports.html",
@@ -19,8 +18,9 @@ import io.cucumber.testng.CucumberOptions;
 			monochrome=false,  //console output colour
 
 
-			tags = "@tagTree", //tags from feature file
+		//	tags = "@TC_TE_001", //tags from feature file
 			features = {"src/test/resources/features"}, //location of feature files
+				//features = {"@target/failedtc.txt"},
 			glue= {"stepdefinition","com.apphooks"}) //location of step definition files
 
 
